a5dc6148a1dfd0d69ed53ac992cd6a12da60e3df
PSP369 <156859865+PSP369@users.noreply.github.com>
2024-02-13 21:01:09 -0500

    added a new animal
    added lion

---
+++ PetsOOP/ClassLibrary1/lion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class lion : Pet
+    {
+        public lion(string name) : base(name)
+        {
+
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " Roars at you.");
+        }
+        public void HideInshell()
+        {
+            Console.WriteLine(Name + " Hides in his Cave.");
+        }
+    }
+}
